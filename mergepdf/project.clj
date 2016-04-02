fa1bcbe3052c87534453480df437f32d0ae2f755
Paul Gowder <paul.gowder@gmail.com>
2016-04-02 13:22:59 -0500

    cleanup and make binary

--- mergepdf/project.clj
+++ mergepdf/project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"] [pdfboxing "0.1.9"]]
-  :main ^:skip-aot mergepdf.core
+  :main mergepdf.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
